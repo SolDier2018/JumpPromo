6dd71762631b7b996dae4a25dc2bc4fdc6600a91
e.manuylov@justlook.ru <Joi1032013>
2019-11-28 15:35:08 +0700

    добавляет динамики компоненту <Filter />

--- src/app/Orders/components/OrdersList.jsx
+++ src/app/Orders/components/OrdersList.jsx
@@ -30,15 +30,25 @@ class OrdersList extends React.Component {
 
         const button = {
             button: [
-                <button key={'search'} type={'button'} onClick={this.showSearch}><svg width="18" height="18" viewBox="0 0 18 18" fill="#333333"
+                <button
+                    key={'search'}
+                    type={'button'}
+                    onClick={this.showSearch}
+                >
+                    <svg width="18" height="18" viewBox="0 0 18 18" fill="#333333"
                                                                                       xmlns="http://www.w3.org/2000/svg">
                     <path fillRule="evenodd" clipRule="evenodd"
                           d="M12 7C12 9.76142 9.76142 12 7 12C4.23858 12 2 9.76142 2 7C2 4.23858 4.23858 2 7 2C9.76142 2 12 4.23858 12 7ZM10.908 12.8084C9.79193 13.5608 8.44725 14 7 14C3.13401 14 0 10.866 0 7C0 3.13401 3.13401 0 7 0C10.866 0 14 3.13401 14 7C14 8.70049 13.3936 10.2594 12.3854 11.4722C12.3951 11.4812 12.4048 11.4905 12.4143 11.5L17.0001 16.0858C17.3906 16.4763 17.3906 17.1095 17.0001 17.5C16.6096 17.8905 15.9764 17.8905 15.5859 17.5L11.0001 12.9142C10.9665 12.8806 10.9358 12.8453 10.908 12.8084Z"
                           fill=""/>
-                </svg></button>
+                </svg>
+                </button>
             ],
             filter: [
-                <Filter key={'filter'} onClick={() => console.log('---')}/>
+                <Filter
+                    key={'filter'}
+                    label={'Не обработанные'}
+                    onClick={() => console.log('---')}
+                />
             ]
         };
 
--- src/containers/listHead/Filter.jsx
+++ src/containers/listHead/Filter.jsx
@@ -1,18 +1,54 @@
-import React from 'react';
-import style from './listHead.module.css';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Button} from '../../ui/button/button';
 
-export default function({...attrs}) {
-  return(
-      <div className={style.contentMenuFilter}>
-          <button type={'button'} {...attrs}>
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="#333333"
-                   xmlns="http://www.w3.org/2000/svg">
-                  <path fillRule="evenodd" clipRule="evenodd"
-                        d="M2 4.99999H14V5.99999H2V4.99999ZM4 7.99999H12V8.99999H4V7.99999ZM10 11H6V12H10V11Z"
-                        fill=""/>
-              </svg>
-              <p>Не обработанные</p>
-          </button>
-      </div>
-  )
-};
+import style from './listHead.module.css';
+import kit from "../../app/kit.module.css";
+
+class Filter extends Component {
+
+    static defaultProps = {
+      label: 'Передайте props - label',
+      onClick: () => console.error('Передайте props - onClick')
+    };
+
+    static propTypes = {
+      label: PropTypes.string,
+      onClick: PropTypes.func
+    };
+
+    state = {
+        active: false
+    };
+
+    handleClick = () => {
+        const {onClick} = this.props;
+        onClick();
+        this.setState({
+            active: !this.state.active
+        });
+    };
+
+    render() {
+        const {label} = this.props;
+        const {active} = this.state;
+        return(
+            <div className={style.contentMenuFilter + ' ' + (active ? style.activeFilter : '')}>
+                <Button
+                    type={'button'}
+                    className={kit.transparent + ' ' + style.buttonFilter}
+                    onClick={this.handleClick}
+                >
+                    <svg width="16" height="16" viewBox="0 0 16 16" fill="#333333"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" clipRule="evenodd"
+                              d="M2 4.99999H14V5.99999H2V4.99999ZM4 7.99999H12V8.99999H4V7.99999ZM10 11H6V12H10V11Z"
+                              fill=""/>
+                    </svg>
+                    <p>{label}</p>
+                </Button>
+            </div>
+        )
+    }
+}
+export default Filter;
